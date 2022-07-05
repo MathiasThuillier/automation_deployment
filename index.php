bf493c8eca2a6b024aa7ff6e72fe69a82836e413
Mathias.Thuillier <mathias.thuillier@epsi.fr>
2022-07-05 13:16:00 +0200

    Ajout des premiers fichiers

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- En-tête de la page -->
+        <meta charset="utf-8" />
+        <title>Approche DevOps</title>
+    </head>
+
+    <body>
+        <h1>
+            Test push site devops
+        </h1>
+    </body>
+</html>
